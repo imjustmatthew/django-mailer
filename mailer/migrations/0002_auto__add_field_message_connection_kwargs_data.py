1adff054b7564e7693aa4f6a392a1cf9656c5f9a
Trey Hunner <treyhunner@gmail.com>
2011-02-04 00:18:12 -0800

    Added South migration for new connection_kwargs_data field

---
+++ mailer/migrations/0002_auto__add_field_message_connection_kwargs_data.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Message.connection_kwargs_data'
+        db.add_column('mailer_message', 'connection_kwargs_data', self.gf('django.db.models.fields.TextField')(default=''), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Message.connection_kwargs_data'
+        db.delete_column('mailer_message', 'connection_kwargs_data')
+
+
+    models = {
+        'mailer.dontsendentry': {
+            'Meta': {'object_name': 'DontSendEntry'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'to_address': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'when_added': ('django.db.models.fields.DateTimeField', [], {})
+        },
+        'mailer.message': {
+            'Meta': {'object_name': 'Message'},
+            'connection_kwargs_data': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message_data': ('django.db.models.fields.TextField', [], {}),
+            'priority': ('django.db.models.fields.CharField', [], {'default': "'2'", 'max_length': '1'}),
+            'when_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        },
+        'mailer.messagelog': {
+            'Meta': {'object_name': 'MessageLog'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'log_message': ('django.db.models.fields.TextField', [], {}),
+            'message_data': ('django.db.models.fields.TextField', [], {}),
+            'priority': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'result': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'when_added': ('django.db.models.fields.DateTimeField', [], {}),
+            'when_attempted': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        }
+    }
+
+    complete_apps = ['mailer']
